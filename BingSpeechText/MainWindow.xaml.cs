ef057b91e28399239cd2609bdc291992beaa3732
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-11-12 08:59:45 -0300

    Cambios en proyecto speech to tet

--- BingSpeechText/MainWindow.xaml.cs
+++ BingSpeechText/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
+using System.Threading;
+using System.Windows.Threading;
+using Microsoft.CognitiveServices.SpeechRecognition;
 
 namespace BingSpeechText
 {
@@ -20,9 +24,52 @@ namespace BingSpeechText
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MicrophoneRecognitionClient micClient;
         public MainWindow()
         {
             InitializeComponent();
+            this.micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(SpeechRecognitionMode.ShortPhrase, "en-US", "1d802aaea3b54dc5a18fd04014ae490b");
+            this.micClient.OnMicrophoneStatus += MicClient_OnMicrophoneStatus;
+            this.micClient.OnResponseReceived += MicClient_OnResponseReceived;
+        }
+
+        private async void MicClient_OnResponseReceived(object sender, SpeechResponseEventArgs e)
+        {
+            if(e.PhraseResponse.Results.Length > 0)
+            {
+                await Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                    new Action(() =>
+                    {
+                        this.MySpeechResponse.Text = $"'{e.PhraseResponse.Results[0].DisplayText}',";
+                        this.MySpeechResponseConfidence.Text = $"confidence:{e.PhraseResponse.Results[0].Confidence}";
+                        //this.Speak(this.MySpeechResponse.Text);
+                    }));
+            }
+        }
+
+        private void MicClient_OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                new Action(() =>
+                   {
+                       if (e.Recording)
+                       {
+                           this.status.Text = "Escuchando...";
+                           this.RecordingBar.Visibility = Visibility.Visible;
+                       }
+                       else
+                       {
+                           this.status.Text = "No Escuchando...";
+                           this.RecordingBar.Visibility = Visibility.Collapsed;
+                       }
+                   }));
+        }
+
+        private void button_click(object sender, RoutedEventArgs e)
+        {
+            this.MySpeechResponse.Text = string.Empty;
+            this.MySpeechResponseConfidence.Text = string.Empty;
+            this.micClient.StartMicAndRecognition();
         }
     }
 }
